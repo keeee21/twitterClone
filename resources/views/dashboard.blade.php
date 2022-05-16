12aa4ed276de52bc89e17324149f671435005fe8
Keisuke Uemoto <keisuke.u211016@gmail.com>
2022-05-16 16:43:35 +0900

    余分な行の削除、input type修正

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <div>
-                <input type="keyword" name="keyword" required class="block p-2.5 w-full text-sm text-gray-900 bg-white-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="キーワードを入力してください">
+                <input type="text" name="keyword" required class="block p-2.5 w-full text-sm text-gray-900 bg-white-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="キーワードを入力してください">
             </div>
         </form>
     </div>
--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -29,7 +29,6 @@
                         <button type="submit" class="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit" value="ツイートする">ツイートする</button>
                     </div>
                     <div>
-                        <!-- <textarea name="content" id="" cols="20" rows="10" placeholder="今なにしてる？"></textarea> -->
                         <div>
                             <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"></label>
                             <textarea id="content" name="content" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="今なにしてる？"></textarea>
